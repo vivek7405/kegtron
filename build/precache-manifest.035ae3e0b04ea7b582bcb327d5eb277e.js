5a0ae05c79d4758f00c7899c4d10c9118f512887
Vivek <vivek7405@gmail.com>
2020-03-08 16:07:56 +0530

    Add Build Folder for GetForge

---
+++ build/precache-manifest.035ae3e0b04ea7b582bcb327d5eb277e.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e221032ab061cad83b6ce2bcddbde8",
+    "url": "/assets/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/assets/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "a0e46feb3cc577478b127936e739dd08",
+    "url": "/assets/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "d712b605ed58419c7e6d4ab885d147b7",
+    "url": "/assets/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "2f7ce797cf8f198dedb9a9f38b7ef13b",
+    "url": "/assets/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "ba817517b2c4e1ba1ce802c4d4fafdb4",
+    "url": "/assets/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "d44b28d21e8acf7d7e7f23970893ee76",
+    "url": "/assets/images/Kegtron Cloud Dashboard - bw.png"
+  },
+  {
+    "revision": "e41a19f81922e8339e377be694727965",
+    "url": "/assets/images/Kegtron Cloud Dashboard - white.png"
+  },
+  {
+    "revision": "75ff553ee16b5e186b3428fff88f789e",
+    "url": "/assets/images/Kegtron-White.png"
+  },
+  {
+    "revision": "2b9d7ee8acb3c0dfedfd3fa1a49591ab",
+    "url": "/assets/images/KegtronLogoBlack.png"
+  },
+  {
+    "revision": "f507ac434d73d1f0552ed77228d6e13f",
+    "url": "/assets/images/logo-192x192.png"
+  },
+  {
+    "revision": "f507ac434d73d1f0552ed77228d6e13f",
+    "url": "/assets/images/logo-512x512.png"
+  },
+  {
+    "revision": "ab0c17d4408e108e29b4",
+    "url": "/bundle.ab0c1.js"
+  },
+  {
+    "revision": "ab0c17d4408e108e29b4",
+    "url": "/bundle.b3cba.css"
+  },
+  {
+    "revision": "ce12866d7d5bcac1221e3d226c63ff5f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2384d7fe08006dd68f99",
+    "url": "/polyfills.2384d.js"
+  },
+  {
+    "revision": "2c49da9d54205ac3c6bb",
+    "url": "/route-account.chunk.2c49d.js"
+  },
+  {
+    "revision": "2c49da9d54205ac3c6bb",
+    "url": "/route-account.chunk.42144.css"
+  },
+  {
+    "revision": "7a6338f4756644ffb224",
+    "url": "/route-add-device.chunk.6b2e7.css"
+  },
+  {
+    "revision": "7a6338f4756644ffb224",
+    "url": "/route-add-device.chunk.7a633.js"
+  },
+  {
+    "revision": "1f2832e0bf773da3eb31",
+    "url": "/route-keg.chunk.1f283.js"
+  },
+  {
+    "revision": "1f2832e0bf773da3eb31",
+    "url": "/route-keg.chunk.a75e0.css"
+  },
+  {
+    "revision": "c4ff0c399f8c408e4eb7",
+    "url": "/route-keg~route-reports.chunk.c4ff0.js"
+  },
+  {
+    "revision": "c95301a92653bd59bc37",
+    "url": "/route-login.chunk.0f5e4.css"
+  },
+  {
+    "revision": "c95301a92653bd59bc37",
+    "url": "/route-login.chunk.c9530.js"
+  },
+  {
+    "revision": "365c265c36f9f1bfcda4",
+    "url": "/route-reports.chunk.365c2.js"
+  },
+  {
+    "revision": "365c265c36f9f1bfcda4",
+    "url": "/route-reports.chunk.c8567.css"
+  },
+  {
+    "revision": "461921e90e3fc0882708",
+    "url": "/route-settings.chunk.1d24e.css"
+  },
+  {
+    "revision": "461921e90e3fc0882708",
+    "url": "/route-settings.chunk.46192.js"
+  },
+  {
+    "revision": "e74ada965d7642d76521",
+    "url": "/route-site.chunk.4344c.css"
+  },
+  {
+    "revision": "e74ada965d7642d76521",
+    "url": "/route-site.chunk.e74ad.js"
+  },
+  {
+    "revision": "74a818f19a140ea9c59e",
+    "url": "/route-sites.chunk.74a81.js"
+  },
+  {
+    "revision": "74a818f19a140ea9c59e",
+    "url": "/route-sites.chunk.a389d.css"
+  },
+  {
+    "revision": "13b82ed03d648451c57be206f75f0216",
+    "url": "/sw-debug.js"
+  },
+  {
+    "revision": "aaa58e29f0b1eb42cc5aa4d023cfa0f0",
+    "url": "/sw-esm.js"
+  },
+  {
+    "revision": "beec18519751ae79bfb6c12cb9a3246f",
+    "url": "/sw.js"
+  },
+  {
+    "revision": "949448631310325a7db4",
+    "url": "/vendors~route-account~route-add-device~route-keg~route-login~route-reports~route-settings~route-site~b91c8e5d.chunk.94944.js"
+  }
+]);
---
+++ build/precache-manifest.29d6a36ae44a4ab69858f160b930f9e3.esm.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e221032ab061cad83b6ce2bcddbde8",
+    "url": "/assets/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/assets/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "a0e46feb3cc577478b127936e739dd08",
+    "url": "/assets/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "d712b605ed58419c7e6d4ab885d147b7",
+    "url": "/assets/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "2f7ce797cf8f198dedb9a9f38b7ef13b",
+    "url": "/assets/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "ba817517b2c4e1ba1ce802c4d4fafdb4",
+    "url": "/assets/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "d44b28d21e8acf7d7e7f23970893ee76",
+    "url": "/assets/images/Kegtron Cloud Dashboard - bw.png"
+  },
+  {
+    "revision": "e41a19f81922e8339e377be694727965",
+    "url": "/assets/images/Kegtron Cloud Dashboard - white.png"
+  },
+  {
+    "revision": "75ff553ee16b5e186b3428fff88f789e",
+    "url": "/assets/images/Kegtron-White.png"
+  },
+  {
+    "revision": "2b9d7ee8acb3c0dfedfd3fa1a49591ab",
+    "url": "/assets/images/KegtronLogoBlack.png"
+  },
+  {
+    "revision": "f507ac434d73d1f0552ed77228d6e13f",
+    "url": "/assets/images/logo-192x192.png"
+  },
+  {
+    "revision": "f507ac434d73d1f0552ed77228d6e13f",
+    "url": "/assets/images/logo-512x512.png"
+  },
+  {
+    "revision": "5062fb76b6c4646bedb0925e5bf9c97c",
+    "url": "/bundle.52ef2.esm.js"
+  },
+  {
+    "revision": "ab0c17d4408e108e29b4",
+    "url": "/bundle.b3cba.css"
+  },
+  {
+    "revision": "ce12866d7d5bcac1221e3d226c63ff5f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4fa42edb1065449ee75eae1337b0338",
+    "url": "/polyfills.e23b5.esm.js"
+  },
+  {
+    "revision": "2c49da9d54205ac3c6bb",
+    "url": "/route-account.chunk.42144.css"
+  },
+  {
+    "revision": "f20475fe945ddc5accce7f3c88b2319d",
+    "url": "/route-account.chunk.dc04b.esm.js"
+  },
+  {
+    "revision": "7a6338f4756644ffb224",
+    "url": "/route-add-device.chunk.6b2e7.css"
+  },
+  {
+    "revision": "51782bfeb3c173db1807098964c77fd6",
+    "url": "/route-add-device.chunk.f2446.esm.js"
+  },
+  {
+    "revision": "1f2832e0bf773da3eb31",
+    "url": "/route-keg.chunk.a75e0.css"
+  },
+  {
+    "revision": "57541095372faac6eb3ff9ceaf0e0063",
+    "url": "/route-keg.chunk.d4398.esm.js"
+  },
+  {
+    "revision": "3aaf6c5a3c073e9c5680953d4b7992f8",
+    "url": "/route-keg~route-reports.chunk.a5e77.esm.js"
+  },
+  {
+    "revision": "c95301a92653bd59bc37",
+    "url": "/route-login.chunk.0f5e4.css"
+  },
+  {
+    "revision": "89185af9573669522b87c6ab06eef3f7",
+    "url": "/route-login.chunk.730c1.esm.js"
+  },
+  {
+    "revision": "157db73ffa00ac83bdf245bcc75c063b",
+    "url": "/route-reports.chunk.16386.esm.js"
+  },
+  {
+    "revision": "365c265c36f9f1bfcda4",
+    "url": "/route-reports.chunk.c8567.css"
+  },
+  {
+    "revision": "461921e90e3fc0882708",
+    "url": "/route-settings.chunk.1d24e.css"
+  },
+  {
+    "revision": "ebdeeeafd96666ad8561cd069dcf21f8",
+    "url": "/route-settings.chunk.f9ee9.esm.js"
+  },
+  {
+    "revision": "1bfccb76fdd22cbcce508a6a8f014a2d",
+    "url": "/route-site.chunk.1e371.esm.js"
+  },
+  {
+    "revision": "e74ada965d7642d76521",
+    "url": "/route-site.chunk.4344c.css"
+  },
+  {
+    "revision": "806004c5427f9f582e73bbacb1907e7d",
+    "url": "/route-sites.chunk.8cd25.esm.js"
+  },
+  {
+    "revision": "74a818f19a140ea9c59e",
+    "url": "/route-sites.chunk.a389d.css"
+  },
+  {
+    "revision": "4d229e49662b895a823858ff4f55c9a9",
+    "url": "/vendors~route-account~route-add-device~route-keg~route-login~route-reports~route-settings~route-site~b91c8e5d.chunk.94944.esm.js"
+  }
+]);
